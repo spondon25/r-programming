1bb085ec152618a1be6bfc5632931a2c22f73836
Asif Faisal Spondon <spondon171@gmail.com>
2025-03-09 22:17:25 +0600

    Add files via upload

---
+++ Base_R/R as a calculator/R_as_calculator.R
@@ -0,0 +1,18 @@
+# R as a calculator
+
+2 + 4
+10/3
+2 * 4
+# Similar to a calculator, there are symbols for other arithmetic operations.
+# E.g. -(subtraction), /(division), *(multiplication), etc.
+
+
+((3 + 12)/3 + 8)
+##Parentheses "(" and ")" are used to define arithmetic expressions, and they must be matched; unmatched parentheses will result in error.
+
+
+{10 + 2}+ 5
+##Using curly brackets "{" and "}" do not result in any error but should not be used as they have some specific uses in R, e.g. defining a function.
+
+
+##The square brackets "[" and "]" cannot be used in arithmetic expressions.
