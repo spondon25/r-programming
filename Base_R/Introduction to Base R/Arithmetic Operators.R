b206709e2eab71d3c12613024137256b51b484dc
Asif Faisal Spondon <spondon171@gmail.com>
2025-03-27 01:50:41 +0600

    Add files via upload

---
+++ Base_R/Introduction to Base R/Arithmetic Operators.R
@@ -0,0 +1,38 @@
+#Arithmetic Operators
+
+#operation & operator
+#addition +
+#subtraction -
+#multiplication *
+#division /
+#power ^
+#integer division %/%
+#module %%
+
+#logical
+#equal to ==
+#greater or greater equal >, >=
+#less or less equal <, <=
+#not equal !=
+
+#Example
+10 + 2
+# [1] 12
+x <- 10 
+x
+# [1] 10
+y <- 2
+x^y
+# [1] 100
+x/y
+# [1] 5
+10 %/% 3
+# [1] 3
+10 %% 3
+# [1] 1
+x == 11
+# [1] FALSE
+13 >= 130
+# [1] FALSE
+
+
