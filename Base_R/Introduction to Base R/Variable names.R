85145cbdc8d6cef88be66e9605f9052c451799a9
Asif Faisal Spondon <spondon171@gmail.com>
2025-03-27 02:06:00 +0600

    Update Variable names.R

--- Base_R/Introduction to Base R/Variable names.R
+++ Base_R/Introduction to Base R/Variable names.R
@@ -1,10 +1,10 @@
-#Variable names
+# Variable names
 
-#In R, variable names are case sensitive, and a valid variable name (syntactic name) consists of a combination of
+# In R, variable names are case sensitive, and a valid variable name (syntactic name) consists of a combination of
 
-#(a-z, A-Z), (0-9), (.), (-), (_)
-#A variable name cannot start with a number or a hyphen or an underscore and can start with a dot, but it must be followed by a letter
-#A non-syntactic name should be around backticks
+# (a-z, A-Z), (0-9), (.), (-) and (_)
+# A variable name cannot start with a number or a hyphen or an underscore and can start with a dot, but it must be followed by a letter
+# A non-syntactic name should be around backticks
 
-#A good programming practice is to use meaningful variable names in the codes, and self-explanatory variable names increase the readability of the codes
-#Existing R functions, names, or words (e.g. mean log, exp, TRUE, c, etc.) should not be used as variable names
+# A good programming practice is to use meaningful variable names in the codes, and self-explanatory variable names increase the readability of the codes
+# Existing R functions, names, or words (e.g. mean log, exp, TRUE, c, etc.) should not be used as variable names
