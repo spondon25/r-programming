e8f87ce5af89efd02298b5ac69612c9a84570a4a
Asif Faisal Spondon <spondon171@gmail.com>
2025-03-27 02:07:04 +0600

    Update Data type.R

--- Base_R/Introduction to Base R/Data type.R
+++ Base_R/Introduction to Base R/Data type.R
@@ -1,26 +1,26 @@
-#Data type
+# Data type
 
-#There are three major data types in R
-#numeric
-#(quantitative data)
+# There are three major data types in R
+# numeric
+# (quantitative data)
 
 # Numeric/double 
 xn <- 10
 # Integer
 xi <- 10L
 
-#character
-#(qualitative data)
+# character
+# (qualitative data)
 xc <- "10"
 
-#logical
-#binary data
+# logical
+# binary data
 xl <- TRUE
 
-#The function typeof(x) can be used to identify the type of an R object x
+# The function typeof(x) can be used to identify the type of an R object x
 typeof(xl)
-#[1] "logical"
+# [1] "logical"
 typeof(xn)
-#[1] "double"
+# [1] "double"
 typeof(xi)
-#[1] "integer"
+# [1] "integer"
