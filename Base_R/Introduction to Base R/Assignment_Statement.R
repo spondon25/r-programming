addf7d0e1d15e2229ab496b823a8ca3e6c23c370
Asif Faisal Spondon <spondon171@gmail.com>
2025-03-27 01:16:52 +0600

    Add files via upload

---
+++ Base_R/Introduction to Base R/Assignment_Statement.R
@@ -0,0 +1,14 @@
+##Assignment statement
+
+#Assignment operators "<-" or "=" can be used to assign a value to a variable or a symbol (R object), and it is preferable to use "<-" as "=" has other uses.
+# "<-" is only used to assign a value to a variable
+#object <- value or value -> object
+
+#Assign a value "4" to a variable "x"
+
+x <- 4
+ls()
+#[1] "x"
+
+#R function ls() provides the list of R objects in the current R session
+
