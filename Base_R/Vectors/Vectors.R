9ceeff3e7f8bc49a8866aaae6fe0fb836c7b56f2
Asif Faisal Spondon <spondon171@gmail.com>
2025-04-25 03:44:29 +0600

    Add files via upload

---
+++ Base_R/Vectors/Vectors.R
@@ -0,0 +1,14 @@
+#Vectors
+
+#A variable or an R object with more than one value is known as a vector, and 
+#in R, there are two types of vectors: atomic vectors and lists
+
+#Atomic vector consists of the same type of elements, e.g. a vector with
+#only character values
+
+#List can have elements of di erent data types, i.e. one element of a list
+#could be a numeric value, and the other could be a character values
+
+#Scalar is a vector of length one, and an R object 
+NULL
+#is a vector of length zero
